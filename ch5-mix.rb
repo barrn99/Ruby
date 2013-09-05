9c8632a8b4c95fafb135c6049f6adbd85c7bb386
barrn99 <utopiad2000@yahoo.com>
2013-09-04 21:13:16 -0400

    initial commit

---
+++ ch5-mix.rb
@@ -0,0 +1,39 @@
+var1 = 2
+var2 = '5'
+puts var1 + var2.to_i
+puts var1.to_s + var2
+
+puts''
+puts '15'.to_f
+puts '99.999'.to_f
+puts '99.999'.to_i
+puts ''
+puts '5 is my favorite number!'.to_i
+puts 'Who asked you about 5 or whatever?'.to_i
+puts 'Your momma did.'.to_f
+puts''
+puts 'stringy'.to_s
+puts 3.to_i
+
+puts ''
+#puts 'What\'s your First Name?'
+#first = gets.chomp
+#puts 'What\'s your Middle Name?'
+#middle=gets.chomp
+#puts 'What\'s your Last Name?'
+#last=gets.chomp
+#puts first + ' ' + middle + ' ' + last
+
+#puts 'What\'s your favorite number?'
+#fav1 = gets.chomp
+#fav1_as_integer = fav1.to_i
+#fav2 = fav1_as_integer + 1
+#fav2_as_string = fav2.to_s
+#puts 'I think ' + fav2_as_string + ' is way better!!!!!!'
+
+puts 'What\'s your favorite number?'
+fav1 = gets.chomp
+fav2 = fav1.to_i + 1
+puts 'I think ' + fav2.to_s + ' is way better!!!!'
+
+
---
+++ ch6-fancystrings.rb
@@ -0,0 +1,65 @@
+var1 = 'stop'
+var2 = 'deliver repaid desserts'
+var3 = '....TCELES B HSUP   A magic spell?'
+
+puts var1.reverse
+puts var2.reverse
+puts var3. reverse
+puts var1
+puts var2
+puts var3
+
+puts''
+#puts 'What is your full name?'
+#name = gets.chomp
+#puts 'Did you know there are ' + name.length.to_s + ' characters'
+#puts 'in your name, ' + name + '?'
+
+#puts 'What\'s your first name?'
+#first = gets.chomp
+#puts 'What\'s your middle name?'
+#middle = gets.chomp
+#puts 'What\'s your last name?'
+#last = gets.chomp
+#total = first.length+ middle.length + last.length
+#puts 'You have ' + total.to_s + ' letters in your name!'
+
+#letters = 'aAbBcCdDeE'
+#puts letters.upcase
+#puts letters.downcase
+#puts letters.swapcase
+#puts letters.capitalize
+#puts ' a'.capitalize
+#puts letters
+
+#puts''
+#line_width = 50
+#puts(              'Old Mother Hubbar'.center(line_width))
+#puts('Sat in her cupboard'.center(line_width))
+#puts('Eating her curds and whey,'.center(line_width))
+#puts('When along came a spider'.center(line_width))
+#puts('Who sat down beside her'.center(line_width))
+#puts('And scared her poor shoe dog away.'.center(line_width))
+
+#line_width = 40
+#str = '--> text <--'
+#puts(str.ljust( line_width))
+#puts(str.center(line_width))
+#puts(str.rjust( line_width))
+#puts(str.ljust(line_width/2) + str.rjust(line_width/2))
+
+puts''
+#puts 'What do you want?'
+#want = gets.chomp
+#puts 'Whaddaya Mean "' + want + '"?!?' ' You\'re fired!!'
+
+line_width = 60
+puts('Table of Contents'.center(line_width))
+puts''
+puts('Chapter 1:  Getting Started'.ljust(line_width/2) + 'Page  1'.rjust(line_width/2))
+puts('Chapter 2:  Numbers'.ljust(line_width/2) + 'Page  9'.rjust(line_width/2))
+puts('Chapter 3:  Letters'.ljust(line_width/2) + 'Page 13'.rjust(line_width/2))
+
+
+
+
