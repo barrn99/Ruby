7c15f83961eef9dfe184e144a50054fde4c919ac
barrn99 <utopiad2000@yahoo.com>
2013-09-05 20:36:48 -0400

    modified:   ch8-Table.rb

--- ch8-Table.rb
+++ ch8-Table.rb
@@ -1,8 +1,9 @@
-table = ['Chapter 1:  Getting Started','page  1','Chapter 2:  Numbers','page  9','Chapter 3:  Letters','page 13']
+table = ['Chapter 1:  Getting Started','Chapter 2:  Numbers','Chapter 3:  Letters']
+page = ['Page  1','Page  9','Page 13']
 line_width = 70
 puts 'Table of Contents'.center(line_width)
 
 puts ''
-puts table[0].ljust(line_width/2) + table[1].rjust(line_width/2)
-puts table[2].ljust(line_width/2) + table[3].rjust(line_width/2)
-puts table[4].ljust(line_width/2) + table[5].rjust(line_width/2)
+table.zip(page).each do |table, page|
+puts table.ljust(line_width/2) + page.rjust(line_width/2)
+end
